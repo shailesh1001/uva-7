7ee5e285aa898cf36fc9c850e9701dbd9f2bd595
Phillip LeBlanc <pmleblanc007@gmail.com>
2011-10-27 12:12:19 -0500

    starting

---
+++ rounders/round.c
@@ -0,0 +1,24 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEBUG 1
+#define printd if (DEBUG) printf
+
+void run()
+{
+  /**/
+}
+
+int main()
+{
+  int count, j;
+  scanf("%d", &count);
+  for (j=0;j<count;j++)
+  {
+    printd("%d\n",count);
+    run();
+  }
+}
+
+
